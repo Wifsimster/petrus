d83c7b77cd5f030bcf3c07a57548ed9acdedda2a
wifsimster <wifsimster@gmail.com>
2019-05-05 09:50:53 +0200

    Update shows.js

--- shows.js
+++ shows.js
@@ -4,24 +4,24 @@ module.exports = [
   // `Barry`,
   // `Brooklyn Nine Nine`,
   // `Doom Patrol`,
-  `Empire`,
+  // `Empire`,
   // `The Big Bang Theory`,
   // `Legends of tomorrow`,
   // `Into the Badlands`,
   // `One-Punch Man`,
+  // `Cloak Dagger`,
   // `The Blacklist`,
-  // `Warrior`,
   // `Killing Eve`,
   // `The Simpsons`,
   // `Supergirl`,
   // `Game of Thrones`,
   // `The 100`,
   // `The Flash`,
-  `Modern Family`,
-  `Happy`,
-  `Strike Back`
+  // `Modern Family`
+  // `Happy`,
+  // `Strike Back`,
   // `You re the Worst,`
-  // `Cloak Dagger`,
   // `Young Sheldon`,
-  // `iZombie`
+  // `iZombie`,
+  // `Warrior`
 ].sort()
