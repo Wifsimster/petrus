2547c5e520de54dd8f0b157565ac03a16171d348
Wifsimster <wifsimster@gmail.com>
2020-08-17 20:07:36 +0200

    fix: new TPB site compliant w/ puppeteer

--- src/petrus.js
+++ src/petrus.js
@@ -2,10 +2,7 @@
  * Petrus
  * Licensed under MIT, https://opensource.org/licenses/MIT/
  */
-import https from 'https'
-import jsdom from 'jsdom'
-
-const { JSDOM } = jsdom
+import puppeteer from 'puppeteer'
 
 class Petrus {
   /**
@@ -23,108 +20,59 @@ class Petrus {
   }
 
   async search(query) {
-    const rows = await this.scrap(query)
-    return this.parseInfo(rows)
+    return await this.scrap(query)
   }
 
   getCategory(val) {
     switch (val) {
     case 'Music':
-      return '0/99/101'
+      return '101'
     case 'Movies':
-      return '0/99/201'
+      return '201'
     case 'TV shows':
-      return '0/99/205'
+      return '205'
     case 'HD - Movies':
-      return '0/99/207'
+      return '207'
     case 'HD - TV shows':
-      return '0/99/208'
+      return '208'
     default:
-      return '0/7/0'
+      return '0'
     }
   }
 
   async scrap(query, category) {
-    return new Promise((resolve, reject) => {
-      const showQuery = encodeURIComponent(query)
-      const categoryURL = this.getCategory(category)
+    // return new Promise((resolve, reject) => {
+    let showQuery = encodeURIComponent(query)
+    let categoryURL = this.getCategory(category)
+      
+    let url = `${this.getBaseUrl()}/search/${showQuery}/${categoryURL}`
+    
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(url)
 
-      https
-        .get(
-          `${this.getBaseUrl()}/search/${showQuery}/${categoryURL}`,
-          response => {
-            const { statusCode, statusMessage } = response
+    const selector = '#torrents li.list-entry'
+    await page.waitForSelector(selector)
 
-            if (statusCode >= 400) {
-              reject({ code: statusCode, message: statusMessage })
-            } else {
-              let data = ''
+    const links = await page.evaluate(selector => {
+      const items = Array.from(document.querySelectorAll(selector))
+      return items.map(item => {
+        return { 
+          category: item.children[0].innerText,
+          name: item.children[1].innerText,
+          uploaded: item.children[2].innerText,
+          magnetLink: item.children[3].children[0].href,
+          size: item.children[4].innerText,
+          seeders: parseInt(item.children[5].innerText),
+          leechers: parseInt(item.children[6].innerText),
+          author: item.children[7].innerText
+        }
+      })
+    }, selector)
 
-              response.on('data', chunk => {
-                data += chunk
-              })
+    await browser.close()
 
-              response.on('end', () => {
-                const fragment = JSDOM.fragment(data)
-                const selector = fragment.querySelectorAll('tr')
-                let array = []
-
-                for (let i = 0; i < selector.length; i++) {
-                  array.push(selector[i].innerHTML)
-                }
-
-                resolve(array)
-              })
-            }
-          }
-        )
-        .on('error', e => {
-          console.error(e)
-          reject(e)
-        })
-    })
-  }
-
-  static getQuality(row) {
-    let matchTvShowQuality = /href="\/browse\/205[\S]*"/g.exec(row)
-    let matchHdTvShowQuality = /href="\/browse\/208[\S]*"/g.exec(row)
-
-    if (matchTvShowQuality) {
-      return 'SD'
-    }
-
-    if (matchHdTvShowQuality) {
-      return 'HD'
-    }
-
-    return ''
-  }
-
-  parseInfo(rows) {
-    let results = rows.map(row => {
-      let matchMagnetLink = /href="(magnet:[\S]+)"\s/g.exec(row)
-      let matchDate = /<font class="[\s\S]+">Uploaded ([\s\S]+)&nbsp;\d/g.exec(
-        row
-      )
-      let matchSize = /<font class="[\s\S]+">[\s\S]+Size ([\s\S]+)&nbsp;([\s\S]+),/g.exec(
-        row
-      )
-      let matchSeeder = /<td align="right">([\d]+)<\/td>/.exec(row)
-      let matchName = /href="\/torrent\/[\d]+\/([\S]+)"/.exec(row)
-
-      return {
-        magnetLink: matchMagnetLink
-          ? matchMagnetLink[1].replace('&amp;', '&')
-          : null,
-        quality: this.getQuality(row),
-        name: matchName ? matchName[1] : null,
-        uploaded: matchDate ? matchDate[1] : null,
-        size: matchSize ? `${matchSize[1]} ${matchSize[2]}` : null,
-        seeder: matchSeeder ? parseInt(matchSeeder[1]) : null
-      }
-    })
-
-    return results.filter(i => i.magnetLink)
+    return links
   }
 }
 
